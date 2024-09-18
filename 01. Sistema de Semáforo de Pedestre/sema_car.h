efc25f23248e1e953ef299303e3a00e432e5b16c
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2024-09-18 12:49:47 -0300

    Update sema_car.h

--- 01. Sistema de Semáforo de Pedestre/sema_car.h
+++ 01. Sistema de Semáforo de Pedestre/sema_car.h
@@ -2,14 +2,24 @@
 #define SEMA_CAR_H
 
 #include "sema.h"
+#include "LiquidCrystal.h"
 
 // Classe que representa o semáforo dos carros
 class SemaCar : public Semaphore {
 private:
+  /* Tempo de início do ciclo amarelo */
+  unsigned long start_yellow;
+
+  // Gerencia o ciclo do led amarelo
+  void handle_yellow();
+
+  void print_count(LiquidCrystal lcd);
+  
   // Gerencia o ciclo de cada um dos leds
   void handle_green() override;
-  void handle_yellow() override;
   void handle_red() override;
+
+  void check_button() override;
 
   // Reinicia o ciclo
   void restart() override;
@@ -18,7 +28,7 @@ public:
   SemaCar();
   
   // Método geral do funcionamento do semáforo
-  void work() override;
+  void work(LiquidCrystal lcd);
 };
 
 #endif
