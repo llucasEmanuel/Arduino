4adcbcb50269b259edb0a2661f409389fa6fba0a
Lucas Emanuel <131834616+llucasEmanuel@users.noreply.github.com>
2024-09-18 12:50:59 -0300

    Update sema.h

--- 01. Sistema de Semáforo de Pedestre/sema.h
+++ 01. Sistema de Semáforo de Pedestre/sema.h
@@ -1,17 +1,24 @@
 #ifndef SEMA_H
 #define SEMA_H
 
+#include "LiquidCrystal.h"
+
 /* Classe abstrata do semáforo */
 class Semaphore {
 protected:
+
+  /* Flag que indica se o contador começou (Quando apertar o botão vai iniciar um contador de 10s para fechar o sinal de carros) */
+  bool is_counting;
+
   /* Tempo de início de cada ciclo de cor do semáforo */
   unsigned long start_green;
-  unsigned long start_yellow;
   unsigned long start_red;
+
+  /* Checa se o botaão do contador foi apertado */
+  virtual void check_button() = 0;
 
 	/* Gerencia o ciclo de cada um dos leds */
   virtual void handle_green() = 0;
-  virtual void handle_yellow() = 0;
   virtual void handle_red() = 0;
 
 	/* Reinicia o ciclo */
@@ -22,7 +29,7 @@ public:
 	Semaphore();
 
 	/* Método geral do funcionamento do semáforo */
-	virtual void work() = 0;
+	virtual void work(LiquidCrystal lcd) = 0;
 };
 
 #endif
